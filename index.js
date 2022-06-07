e61407eee0285d46ade76f94e627d348b5c281fe
Julie Boehm <julie.anne.boehm@gmail.com>
2022-06-07 10:58:14 -0600

    commit tree b5567b514d1c8eaa2d5afa5c1a0422521eca8f08

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(record) {
+    for (const result of record) {
+        console.log(result)
+
+        if (result.result === "W") {
+            return (result.year);
+        }
+    }
+}
+
+record.find(superbowlWin);
